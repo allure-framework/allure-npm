c77ea44bbec4073c85864fe9cf02715ed76e56ba
Boris Osipov <osipov.boris@gmail.com>
2018-11-12 19:10:23 +0300

    Migrate to maven central binary url

--- fetch-source.sh
+++ fetch-source.sh
@@ -7,6 +7,6 @@ mkdir dist
 if [[ -f allure-commandline.tgz ]]; then
     rm allure-commandline.tgz
 fi
-wget  --output-document allure-commandline.tgz https://dl.bintray.com/qameta/generic/io/qameta/allure/allure/$version/allure-$version.tgz
+wget  --output-document allure-commandline.tgz https://repo.maven.apache.org/maven2/io/qameta/allure/allure-commandline/$version/allure-commandline-$version.tgz
 tar -xf allure-commandline.tgz --strip-components=1 --directory dist
 npm test
