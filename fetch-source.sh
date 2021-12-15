0859b8859715c2160e828ff6e703175b513fa67b
Dmitry Baev <baev@users.noreply.github.com>
2021-12-15 16:21:00 +0300

    fix publish

--- fetch-source.sh
+++ fetch-source.sh
@@ -9,4 +9,4 @@ if [[ -f allure-commandline.tgz ]]; then
 fi
 wget  --output-document allure-commandline.tgz https://repo.maven.apache.org/maven2/io/qameta/allure/allure-commandline/$version/allure-commandline-$version.tgz
 tar -xf allure-commandline.tgz --strip-components=1 --directory dist
-
+ls
