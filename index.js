f506308a6366d86ab66b6d22bfdb1b5a0ad8e0af
Dmitry Baev <baev.dm@gmail.com>
2025-12-11 10:58:11 +0000

    add support for white-spaces in path (via #53)

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 #!/usr/bin/env node
-var path = require('path');
-var isWindows = path.sep === '\\';
-var allureCommand = 'allure' + (isWindows ? '.bat' : '');
+var path = require("node:path");
 
-module.exports = function(args) {
-    return require('child_process').spawn(path.join(__dirname, 'dist/bin', allureCommand), args, {
-        env: process.env,
-        stdio: 'inherit',
-        shell: true,
-    });
+function escapeCMDArg(arg) {
+  if (/[\r\n]/.test(arg)) throw new Error("Invalid character in argument");
+  return `"${arg
+    .replace(/\\+(?="|$)/g, "$&$&")
+    .replace(/"/g, '""')
+    .replace(/%/g, "%%cd:~,%")}"`;
 }
+
+module.exports = function (args) {
+  var process = require("node:process");
+  var childProcess = require("node:child_process");
+  if (process.platform === "win32") {
+    let command = path.join(__dirname, "dist/bin/allure.bat");
+    return childProcess.spawn(
+      "cmd.exe",
+      ["/E:ON", "/F:OFF", "/V:OFF", "/d", "/s", "/c", `"${[command, ...args].map(escapeCMDArg).join(" ")}"`],
+      {
+        env: process.env,
+        stdio: "inherit",
+        shell: false,
+        windowsVerbatimArguments: true,
+      },
+    );
+  }
+
+  return childProcess.spawn(path.join(__dirname, "dist/bin/allure"), args, {
+    env: process.env,
+    stdio: "inherit",
+  });
+};
